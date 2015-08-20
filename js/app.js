9eb8ae62d2a744af479c6238506235b1371cb614
Thedischman <milesthedisch@gmail.com>
2015-08-20 17:31:53 +1000

    skroll bug fixed

--- js/app.js
+++ js/app.js
@@ -2,12 +2,9 @@ $(document).ready(function(){
 	var s = skrollr.init();
 
 	$('.slide-container').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 2000,
+
 	});
 
-	
+
 
 })
